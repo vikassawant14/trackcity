7c586b6c31f81f2ed1295873ce25c3ab86bd98cc
vikassawant14 <vickysawant14444@gmail.com>
2026-03-29 14:39:10 +0530

    Update event.js

--- backend/routes/event.js
+++ backend/routes/event.js
@@ -1,1 +1,25 @@
+const express = require("express")
+const router = express.Router()
+const axios = require("axios")
 
+router.get("/:city", async(req,res)=>{
+
+const city = req.params.city
+
+try{
+
+const response = await axios.get(
+`https://app.ticketmaster.com/discovery/v2/events.json?city=${city}&apikey=YOUR_API_KEY`
+)
+
+res.json(response.data)
+
+}catch(err){
+
+res.status(500).json({error:"Event API error"})
+
+}
+
+})
+
+module.exports = router
