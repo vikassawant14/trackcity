6e425180bb2e0908be7ae75d5144f9fb85061f47
vikassawant14 <vickysawant14444@gmail.com>
2026-03-29 12:10:44 +0530

    Create user.js

---
+++ backend/models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+
+name:{
+type:String,
+required:true
+},
+
+email:{
+type:String,
+required:true,
+unique:true
+},
+
+password:{
+type:String,
+required:true
+},
+
+city:{
+type:String,
+default:""
+},
+
+createdAt:{
+type:Date,
+default:Date.now
+}
+
+});
+
+module.exports = mongoose.model("User",UserSchema);
